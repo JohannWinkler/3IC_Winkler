1c27f5b2be5582d97b478577e18ce2f794fdcc8e
Johann.WINKLER <johann.winkler@sbg4.salzburg.at>
2023-03-22 13:05:21 +0100

    Farbige Darstellung Integriert

--- Thread/src/Main.java
+++ Thread/src/Main.java
@@ -7,31 +7,37 @@ public class Main {
     private static final int BUFFER_SIZE = 10; // maximale Größe des Puffers
     private static volatile boolean producerRunning = true; // Status-Flag für den Erzeuger-Thread
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
     public static void main(String[] args) {
         // Erzeuger-Thread starten
         Thread producerThread = new Thread(() -> {
             Random random = new Random();
-            while (producerRunning) {
-                synchronized (buffer) {
-                    // Wenn der Puffer voll ist, warten
-                    while (buffer.size() == BUFFER_SIZE) {
-                        try {
-                            buffer.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+            while (true) {
+                while (producerRunning) {
+                    synchronized (buffer) {
+                        // Wenn der Puffer voll ist, warten
+                        while (buffer.size() == BUFFER_SIZE) {
+                            try {
+                                buffer.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                         }
+                        // Zufallszahl erzeugen und in den Puffer einfügen
+                        int number = random.nextInt(101);
+                        buffer.add(number);
+                        System.out.println(ANSI_GREEN + "Erzeugt: "+ ANSI_RESET + number);
+                        buffer.notify(); // Verbraucher-Thread benachrichtigen
                     }
-                    // Zufallszahl erzeugen und in den Puffer einfügen
-                    int number = random.nextInt(101);
-                    buffer.add(number);
-                    System.out.println("Erzeugt: " + number);
-                    buffer.notify(); // Verbraucher-Thread benachrichtigen
-                }
-                // Pause einlegen
-                try {
-                    Thread.sleep(random.nextInt(1001));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    // Pause einlegen
+                    try {
+                        Thread.sleep(random.nextInt(1001));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
@@ -52,7 +58,7 @@ public class Main {
                     }
                     // Nächste Zahl aus dem Puffer entfernen und ausgeben
                     int number = buffer.remove(0);
-                    System.out.println("Verbraucht: " + number);
+                    System.out.println(ANSI_RED + "Verbraucht: " + ANSI_RESET + number);
                     // Wenn die maximale Anzahl an Zahlen pro Zeile erreicht ist, eine neue Zeile beginnen
                     if (buffer.size() % 10 == 0) {
                         System.out.println();
